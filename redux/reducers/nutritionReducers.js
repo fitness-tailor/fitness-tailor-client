24c967cb7d2df99f6130a87e90a7bf061c38ab4e
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-11-13 17:56:58 -0800

    implement storage and data handling calendar date, recipe list and calories counting related via redux

---
+++ redux/reducers/nutritionReducers.js
@@ -0,0 +1,59 @@
+let nutritionState = {
+  isLoading: false,
+  currentDayFoodList: [],
+  date: null,
+  selectedDate: null,
+  totalCal: 0,
+  errorFetchingJournal: false,
+};
+
+const nutritionReducers = (state = nutritionState, action) => {
+  switch (action.type) {
+    case "FETCH_USER_JOURNAL":
+      return {
+        ...state,
+        isLoading: true,
+        errorFetchingJournal: false,
+        currentDayFoodList: [],
+      };
+
+    case "FETCH_USER_JOURNAL_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        currentDayFoodList: action.payload,
+      };
+
+    case "FETCH_USER_JOURNAL_SUCCESS_BUT_EMPTY":
+      return {
+        ...state,
+        isLoading: false,
+        currentDayFoodList: action.payload,
+      };
+
+    case "FETCH_USER_JOURNAL_ERROR":
+      return {
+        ...state,
+        isLoading: false,
+        errorFetchingJournal: true,
+      };
+
+    case "STORE_CALORIES":
+      return {
+        ...state,
+        totalCal: action.payload,
+      };
+
+    case "STORE_DATES":
+      return {
+        ...state,
+        selectedDate: action.payload.selectedDate,
+        date: action.payload.date,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default nutritionReducers;
--- screens/NutritionScreen.js
+++ screens/NutritionScreen.js
@@ -1,7 +1,7 @@
 import styles from "./styles.js";
 import firebase from "firebase";
 import { connect } from "react-redux";
-import React, { useState, useEffect, Component } from 'react';
+import React, { useState, useEffect, Component } from "react";
 import {
   SafeAreaView,
   AppRegistry,
@@ -9,100 +9,129 @@ import {
   Text,
   View,
   ScrollView,
-  TouchableOpacity
-} from 'react-native';
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
-import moment, { now } from 'moment';
+  TouchableOpacity,
+} from "react-native";
+import { Calendar, CalendarList, Agenda } from "react-native-calendars";
+import moment, { now } from "moment";
 import { render } from "react-dom";
+import {
+  storeDates,
+  getUserJournal,
+} from "../redux/actions/nutritionActions.js";
 import NutritionCard from "./Nutrition_Comp/NutritionCard.js";
 import AddModal from "./Modals/AddModal.js";
 import { AntDesign } from "@expo/vector-icons";
 
 const NutritionScreen = (props) => {
-  const [date, setDate] = useState(null);
-  const [selectedDate, setSelectedDate] = useState(null);
-  const [recipes, setRecipes] = useState([]);
-  const [totalCal, setTotalCal] = useState(null);
+  // const [date, setDate] = useState(null);
+  // const [selectedDate, setSelectedDate] = useState(null);
+  // const [recipes, setRecipes] = useState([]);
+  // const [totalCal, setTotalCal] = useState(null);
   const [addModalVisible, setAddModalVisible] = useState(false);
 
   //run addCalories after every render
-  useEffect(() => {
-    addCalories();
-  })
+  // useEffect(() => {
+  //   addCalories();
+  // });
 
-  const addCalories = () => {
-    let calories = 0;
-    recipes.map((recipe) => {
-      calories += parseInt(recipe[1].calories);
-    })
-    setTotalCal(calories);
-  }
+  // const addCalories = () => {
+  //   let calories = 0;
+  //   recipes.map((recipe) => {
+  //     calories += parseInt(recipe[1].calories);
+  //   });
+  //   setTotalCal(calories);
+  // };
 
-  const displayRecipesOnDate = (date) => {
-    setSelectedDate(date.dateString);
-    let formatted = moment(date.dateString, "YYYY-MM-DD").format("MMMM D, YYYY");
-    setDate(formatted);
-    let yr = date.year;
-    let mm = date.month;
-    let dd = date.day;
-    firebase
-    .database()
-    .ref(`users/${props.displayName}/foodJournal/${yr}/${mm}/${dd}`)
-    .on('value', (snapshot) => {
-      if(snapshot.val() === null) {
-        setRecipes([]);
-        setTotalCal(0);
-      } else {
-        setRecipes(Object.entries(snapshot.val()))
-      }
-    })
-  };
+  // const displayRecipesOnDate = (date) => {
+  //   setSelectedDate(date.dateString);
+  //   let formatted = moment(date.dateString, "YYYY-MM-DD").format(
+  //     "MMMM D, YYYY"
+  //   );
+  //   setDate(formatted);
+  //   let yr = date.year;
+  //   let mm = date.month;
+  //   let dd = date.day;
+  //   firebase
+  //     .database()
+  //     .ref(`users/${props.displayName}/foodJournal/${yr}/${mm}/${dd}`)
+  //     .on("value", (snapshot) => {
+  //       if (snapshot.val() === null) {
+  //         setRecipes([]);
+  //         setTotalCal(0);
+  //       } else {
+  //         setRecipes(Object.entries(snapshot.val()));
+  //       }
+  //     });
+  // };
+
   let addModal = (
     <AddModal
       addModalVisible={addModalVisible}
       setAddModalVisible={setAddModalVisible}
-      date={selectedDate}
+      date={props.selectedDate}
     />
   );
 
+  console.log(props.currentDayFoodList);
+
   return (
     <SafeAreaView style={styles.containerNutScreen}>
-      <View >
+      <View>
         <Calendar
-          onDayPress={(day) => displayRecipesOnDate(day)}
+          onDayPress={(day) => props.getUserJournal(day, props.displayName)}
           markedDates={{
-            [selectedDate] : {selected: true, selectedColor: '#00adf5'},
+            [props.selectedDate]: { selected: true, selectedColor: "#00adf5" },
           }}
           theme={{
             arrowColor: "rgb(22, 66, 92)",
           }}
         />
-        </View>
-        <ScrollView contentContainerStyle={styles.journalNut}>
-            <Text style={[styles.date]}>{date}</Text>
-            <Text style={styles.totalCal}>{totalCal ? `${totalCal} Total Calories` : null}</Text>
-            {recipes.map((recipe, key) => {
-              return (
-                <NutritionCard key={key} id={recipe[0]} name={recipe[1].name} calories={recipe[1].calories} date={date}>
-                </NutritionCard>
-              )
-            })}
-            {date &&
-            <TouchableOpacity
+      </View>
+      <ScrollView contentContainerStyle={styles.journalNut}>
+        <Text style={[styles.date]}>{props.date}</Text>
+        <Text style={styles.totalCal}>
+          {props.totalCal ? `Total Calories: ${props.totalCal}` : null}
+        </Text>
+        {props.currentDayFoodList.map((recipe, key) => {
+          return (
+            <NutritionCard
+              key={key}
+              id={recipe[0]}
+              name={recipe[1].name}
+              calories={recipe[1].calories}
+              date={props.date}
+            ></NutritionCard>
+          );
+        })}
+        {props.date && (
+          <TouchableOpacity
             style={styles.buttonStyles}
             onPress={() => setAddModalVisible(true)}
             activeOpacity="0.5"
-            >
-              <AntDesign name="pluscircle" size={50} color="rgb(37, 93, 120)"/>
-          </TouchableOpacity>}
-          </ScrollView>
-          {addModal}
+          >
+            <AntDesign name="pluscircle" size={50} color="rgb(37, 93, 120)" />
+          </TouchableOpacity>
+        )}
+      </ScrollView>
+      {addModal}
     </SafeAreaView>
   );
 };
 
 const mapStateToProps = (state) => ({
   displayName: state.auth.user.displayName,
+  date: state.nutrition.date,
+  selectedDate: state.nutrition.selectedDate,
+  currentDayFoodList: state.nutrition.currentDayFoodList,
+  totalCal: state.nutrition.totalCal,
 });
 
-export default connect(mapStateToProps)(NutritionScreen);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    storeDates: (date) => dispatch(storeDates(date)),
+    getUserJournal: (date, username) =>
+      dispatch(getUserJournal(date, username)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NutritionScreen);
